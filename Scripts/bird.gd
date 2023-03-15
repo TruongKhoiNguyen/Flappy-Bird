e0528b2d87f102ec82bfaf208106e53a194fcace
Truong Khoi Nguyen <truongkhoi31052002@gmail.com>
2023-03-15 19:40:04 +0700

    feat: basic bird fly

---
+++ Scripts/bird.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _input(event):
+	if event.is_action_pressed("Flap"):
+		linear_velocity.y = -300
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
