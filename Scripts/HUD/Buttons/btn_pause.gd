0b5634afa770e13228bd5b63e0ce7e5ceb64dd28
Truong Khoi Nguyen <truongkhoi31052002@gmail.com>
2023-04-28 15:30:31 +0700

    feat: add return to menu button in pause menu

--- Scripts/HUD/Buttons/btn_pause.gd
+++ Scripts/HUD/Buttons/btn_pause.gd
@@ -3,3 +3,11 @@ extends TextureButton
 
 func _on_pressed():
 	get_tree().paused = true
+
+
+func _on_bird_state_changed(bird: Bird):
+	if (
+			bird.get_bird_state() == bird.STATE.GROUNDED 
+			or bird.get_bird_state() == bird.STATE.HIT
+	):
+		hide()
