3cfe5be273354b88c176e0cd1a5531bb4b545be0
Truong Khoi Nguyen <truongkhoi31052002@gmail.com>
2023-04-30 08:21:33 +0700

    feat: add screen shake when the bird is in HIT or GROUNDED state

---
+++ Scripts/camera.gd
@@ -0,0 +1,31 @@
+extends Camera2D
+
+@export var duration := 0.18
+@export var magnitude := 3
+
+var time := 0.0
+
+func shake():
+	var initial_offset := offset
+	
+	while time < duration:
+		time += get_process_delta_time()
+		time = min(time, duration)
+		
+		var shake_offset := Vector2()
+		shake_offset.x = randi_range(-magnitude, magnitude)
+		shake_offset.y = randi_range(-magnitude, magnitude)
+		
+		offset = initial_offset + shake_offset
+		
+		await get_tree().process_frame
+		
+	offset = initial_offset
+
+
+func _on_bird_state_changed(bird: Bird):
+	if (
+			bird.get_bird_state() == bird.STATE.GROUNDED 
+			or bird.get_bird_state() == bird.STATE.HIT
+	):
+		shake()
